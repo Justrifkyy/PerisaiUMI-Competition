44e242af0973bd677809092813c1370084d362f8
Justrifkyyy <rifky020504@gmail.com>
2025-11-14 09:05:41 +0800

    ok

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -9,17 +9,15 @@ use Illuminate\Support\Facades\Hash;
 
 class AdminUserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+   
     public function run(): void
     {
         User::create([
-            'name' => 'Admin Lontara', // Ganti dengan nama Anda
-            'email' => 'admin@lontara.com', // Ganti dengan email admin
-            'email_verified_at' => now(), // Langsung verifikasi email
-            'password' => Hash::make('password'), // Ganti 'password' dengan password yang kuat
-            'is_admin' => true, // Menandai user ini sebagai admin
+            'name' => 'Admin Lontara', 
+            'email' => 'admin@lontara.com', 
+            'email_verified_at' => now(), 
+            'password' => Hash::make('password'), 
+            'is_admin' => true, 
         ]);
     }
 }
